8a6c6acbc3b4cea2fd01cd5bd6fa02dbcb43d8b2
cgjohn <cgjohn@umich.edu>
2017-01-19 18:12:31 -0500

    added blinking down arrow

---
+++ script.js
@@ -0,0 +1,16 @@
+var mainImg = document.querySelector(".hero-img-cont");
+
+// function swapImg(){
+// 	mainImg.classList.toggle("img-alt");
+// }
+
+
+
+// mainImg.style.backgroundImage = "url(Images/name-logo.png)";
+
+console.log("hello");
+// setInterval(swapImg, 6000);
+
+setInterval(
+	function(){document.querySelector(".down").classList.toggle("blink")
+}, 700);
