c4fb590d98e60d67d79bc554efddd048ca509648
cgjohn <cgjohn@umich.edu>
2017-01-24 00:07:11 -0500

    fixed mobile view of travels section

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ console.log("hello");
 setInterval(
 	function(){document.querySelector(".down").classList.toggle("blink")
 }, 700);
+
+
+var hashTagActive = "";
+
+$(".scroll").on("click" , function (event) {
+    event.preventDefault();
+    //calculate destination place
+    var dest = 0;
+
+    if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+        dest = $(document).height() - $(window).height();
+    } 
+    else {
+        dest = $(this.hash).offset().top;
+    }
+    //go to destination
+    $('html,body').animate({
+        scrollTop: dest
+    }, 1000, 'swing');
+
+    hashTagActive = this.hash;
+});
